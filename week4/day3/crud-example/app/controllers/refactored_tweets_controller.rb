bc897bf98e28ae5f15165422430922c793257853
Justin Herrick <justin@justinherrick.com>
2015-09-16 14:47:31 -0500

    Adding refactored controller.

---
+++ week4/day3/crud-example/app/controllers/refactored_tweets_controller.rb
@@ -0,0 +1,63 @@
+class RefactoredTweetsController < ApplicationController
+
+  def index
+    tweets = Tweet.all
+    render_json(tweets)
+  end
+
+  def show
+    if_tweet do |tweet|
+      render_json(tweet)
+    end
+  end
+
+  def create
+    if not_empty_or_nil?(params[:message])
+      render_json(Tweet.create(message: params[:message]), 201)
+    else
+      render_json_error("The 'message' parameter was empty or not found", 422)
+    end
+  end
+
+  def update
+    if_tweet do |tweet|
+      tweet.message = params[:message]
+      tweet.save
+      render_json(tweet)
+    end
+  end
+
+  def destroy
+    if_tweet do |tweet|
+      tweet.destroy
+      render_json({ message: "Tweet Deleted Successfully." })
+    end
+  end
+
+  private
+
+  def if_tweet
+    if Tweet.exists(params[:id])
+      tweet = Tweet.find(params[:id])
+      yield(tweet)
+    else
+      render_not_found
+    end
+  end
+
+  def not_empty_or_nil?(field)
+    !(field.nil? || field.empty?)
+  end
+
+  def render_json(payload, status: 200)
+    render json: payload.to_json, status: status
+  end
+
+  def render_not_found
+    render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
+  end
+
+  def render_json_error(message, status)
+    render json: { error_msg: message }.to_json, status: status
+  end
+end
